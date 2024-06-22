6d5ae7a4a930d3cd1f71704e3868725c33e5d91f
namooplus <namoo.developer@gmail.com>
2024-06-22 15:29:06 +0900

    feat: refactor lint

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+// @ts-check
+
+import { FlatCompat } from '@eslint/eslintrc';
+import jsLint from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import tsLint, { config } from 'typescript-eslint';
+
+const compat = new FlatCompat();
+
+export default config(
+  // Javascript
+  jsLint.configs.recommended,
+
+  // Typescript
+  ...tsLint.configs.recommended,
+  {
+    name: 'typescript',
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+
+  // Import
+  ...compat.config({
+    plugins: ['import'],
+    rules: {
+      'import/first': 'error',
+      'import/newline-after-import': ['error', { considerComments: true }],
+      'import/no-unresolved': 'off',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal'],
+            ['index', 'parent', 'sibling'],
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+          warnOnUnassignedImports: true,
+        },
+      ],
+    },
+  }),
+
+  // React & Next.js
+  ...compat.config({
+    extends: ['next/core-web-vitals'],
+  }),
+
+  // Prettier
+  prettier,
+);
